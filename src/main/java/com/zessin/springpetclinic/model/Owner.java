4f41a5a2ec5c785ab0d09a9c337c956a44721385
zessin <gabrielzessin@hotmail.com>
2018-12-01 19:48:47 -0200

    Refactoring to multimodule project

--- src/main/java/com/zessin/springpetclinic/model/Owner.java
+++
@@ -1,5 +0,0 @@
-package com.zessin.springpetclinic.model;
-
-public class Owner extends Person {
-
-}
--- src/main/java/com/zessin/springpetclinic/model/Vet.java
+++
@@ -1,5 +0,0 @@
-package com.zessin.springpetclinic.model;
-
-public class Vet extends Person {
-
-}
