ffe904ca64a1cc19a4f84cae638b941bacd7fb93
zessin <gabrielzessin@hotmail.com>
2018-12-10 21:52:30 -0200

    Adding  Vt page and controller

---
+++ pet-clinic-web/src/main/java/com/zessin/springpetclinic/controller/VetController.java
@@ -0,0 +1,14 @@
+package com.zessin.springpetclinic.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class VetController {
+
+	@RequestMapping({"/vet", "/vet/index", "/vet/index.html"})
+	public String listVets() {
+		return "vet/index";
+	}
+
+}
