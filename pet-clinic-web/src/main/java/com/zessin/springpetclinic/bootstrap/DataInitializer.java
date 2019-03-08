7bea924020f4369a7143a85fb90632557cc3d141
zessin <gabrielzessin@hotmail.com>
2019-03-08 14:20:14 -0300

    Adding PetType to DataInitializer

--- pet-clinic-web/src/main/java/com/zessin/springpetclinic/bootstrap/DataInitializer.java
+++ pet-clinic-web/src/main/java/com/zessin/springpetclinic/bootstrap/DataInitializer.java
@@ -4,8 +4,10 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
 import com.zessin.springpetclinic.model.Owner;
+import com.zessin.springpetclinic.model.PetType;
 import com.zessin.springpetclinic.model.Vet;
 import com.zessin.springpetclinic.service.OwnerService;
+import com.zessin.springpetclinic.service.PetTypeService;
 import com.zessin.springpetclinic.service.VetService;
 
 @Component
@@ -13,14 +15,24 @@ public class DataInitializer implements CommandLineRunner {
 
 	private final OwnerService ownerService;
 	private final VetService vetService;
+	private final PetTypeService petTypeService;
 
-	public DataInitializer(OwnerService ownerService, VetService vetService) {
+	public DataInitializer(OwnerService ownerService, VetService vetService, PetTypeService petTypeService) {
 		this.ownerService = ownerService;
 		this.vetService = vetService;
+		this.petTypeService = petTypeService;
 	}
 
 	@Override
 	public void run(String... args) throws Exception {
+
+		PetType dog = new PetType();
+		dog.setName("Dog");
+		petTypeService.save(dog);
+
+		PetType cat = new PetType();
+		cat.setName("Cat");
+		petTypeService.save(cat);
 
 		Owner owner1 = new Owner();
 		owner1.setFirstName("Joao");
