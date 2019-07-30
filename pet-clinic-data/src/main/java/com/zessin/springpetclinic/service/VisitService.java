8338f82d0ae6ac726041f286b1b4898d186e444d
zessin <gabrielzessin@hotmail.com>
2019-07-29 23:30:15 -0300

    Adding map service for Visit

---
+++ pet-clinic-data/src/main/java/com/zessin/springpetclinic/service/VisitService.java
@@ -0,0 +1,7 @@
+package com.zessin.springpetclinic.service;
+
+import com.zessin.springpetclinic.model.Visit;
+
+public interface VisitService extends CrudService<Visit, Long> {
+
+}
---
+++ pet-clinic-data/src/main/java/com/zessin/springpetclinic/service/map/VisitMapService.java
@@ -0,0 +1,43 @@
+package com.zessin.springpetclinic.service.map;
+
+import java.util.Set;
+
+import org.springframework.stereotype.Service;
+
+import com.zessin.springpetclinic.model.Visit;
+import com.zessin.springpetclinic.service.VisitService;
+
+@Service
+public class VisitMapService extends AbstractMapService<Visit, Long> implements VisitService {
+
+	@Override
+	public Set<Visit> findAll() {
+		return super.findAll();
+	}
+
+	@Override
+	public Visit findById(Long id) {
+		return super.findById(id);
+	}
+
+	@Override
+	public Visit save(Visit object) {
+		if (object == null || object.getPet() == null || object.getPet().getId() == null ||
+			object.getPet().getOwner() == null || object.getPet().getOwner().getId() == null) {
+			throw new RuntimeException("Invalid visit");
+		}
+
+		return super.save(object);
+	}
+
+	@Override
+	public void delete(Visit object) {
+		super.delete(object);
+	}
+
+	@Override
+	public void deleteById(Long id) {
+		super.deleteById(id);
+	}
+
+}
