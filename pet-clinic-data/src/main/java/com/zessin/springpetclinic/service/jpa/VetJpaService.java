1e688e008f34ba9041645dd1b2e90172fb747fb5
zessin <gabrielzessin@hotmail.com>
2019-07-29 23:13:29 -0300

    Adding JPA service for Vet

---
+++ pet-clinic-data/src/main/java/com/zessin/springpetclinic/service/jpa/VetJpaService.java
@@ -0,0 +1,52 @@
+package com.zessin.springpetclinic.service.jpa;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import com.zessin.springpetclinic.model.Vet;
+import com.zessin.springpetclinic.repository.VetRepository;
+import com.zessin.springpetclinic.service.VetService;
+
+@Service
+@Profile("jpa")
+public class VetJpaService implements VetService {
+
+	private final VetRepository vetRepository;
+
+	public VetJpaService(VetRepository vetRepository) {
+		this.vetRepository = vetRepository;
+	}
+
+	@Override
+	public Set<Vet> findAll() {
+		Set<Vet> vets = new HashSet<>();
+
+		vetRepository.findAll().forEach(vets::add);
+
+		return vets;
+	}
+
+	@Override
+	public Vet findById(Long id) {
+		return vetRepository.findById(id).orElse(null);
+	}
+
+	@Override
+	public Vet save(Vet object) {
+		return vetRepository.save(object);
+	}
+
+	@Override
+	public void delete(Vet object) {
+		vetRepository.delete(object);
+	}
+
+	@Override
+	public void deleteById(Long id) {
+		vetRepository.deleteById(id);
+	}
+
+}
