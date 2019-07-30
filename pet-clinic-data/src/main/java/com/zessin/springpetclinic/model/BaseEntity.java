b6ad178efa6bbee17ec12d93c15d6233c739cb23
zessin <gabrielzessin@hotmail.com>
2019-07-29 22:40:07 -0300

    Adding JPA to BaseEntity

--- pet-clinic-data/src/main/java/com/zessin/springpetclinic/model/BaseEntity.java
+++ pet-clinic-data/src/main/java/com/zessin/springpetclinic/model/BaseEntity.java
@@ -1,7 +1,15 @@
 package com.zessin.springpetclinic.model;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+
+@MappedSuperclass
 public class BaseEntity {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 
 	public Long getId() {
