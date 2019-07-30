cce0b1b64e13f6f2ca7e4dd0b052fbc5b0bcf924
zessin <gabrielzessin@hotmail.com>
2019-07-29 23:10:42 -0300

    Adding JPA service for Owner

---
+++ pet-clinic-data/src/main/java/com/zessin/springpetclinic/service/jpa/OwnerJpaService.java
@@ -0,0 +1,57 @@
+package com.zessin.springpetclinic.service.jpa;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import com.zessin.springpetclinic.model.Owner;
+import com.zessin.springpetclinic.repository.OwnerRepository;
+import com.zessin.springpetclinic.service.OwnerService;
+
+@Service
+@Profile("jpa")
+public class OwnerJpaService implements OwnerService {
+
+	private final OwnerRepository ownerRepository;
+
+	public OwnerJpaService(OwnerRepository ownerRepository) {
+		this.ownerRepository = ownerRepository;
+	}
+
+	@Override
+	public Set<Owner> findAll() {
+		Set<Owner> owners = new HashSet<>();
+
+		ownerRepository.findAll().forEach(owners::add);
+
+		return owners;
+	}
+
+	@Override
+	public Owner findById(Long id) {
+		return ownerRepository.findById(id).orElse(null);
+	}
+
+	@Override
+	public Owner save(Owner object) {
+		return ownerRepository.save(object);
+	}
+
+	@Override
+	public void delete(Owner object) {
+		ownerRepository.delete(object);
+	}
+
+	@Override
+	public void deleteById(Long id) {
+		ownerRepository.deleteById(id);
+	}
+
+	@Override
+	public Owner findByLastName(String lastName) {
+		return ownerRepository.findByLastName(lastName);
+	}
+
+}
