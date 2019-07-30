a0b7bcd08ca9cb062a2962613ffe1db4e711e58b
zessin <gabrielzessin@hotmail.com>
2019-07-29 22:55:46 -0300

    Adding JPA to Visit entity

--- pet-clinic-data/src/main/java/com/zessin/springpetclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/com/zessin/springpetclinic/model/Pet.java
@@ -1,11 +1,15 @@
 package com.zessin.springpetclinic.model;
 
 import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 @Entity
@@ -25,6 +29,9 @@ public class Pet extends BaseEntity {
 
 	@Column(name = "birth_date")
 	private LocalDate birthDate;
+
+	@OneToMany(cascade = CascadeType.ALL, mappedBy = "pet")
+	private Set<Visit> visits = new HashSet<>();
 
 	public String getName() {
 		return name;
@@ -56,6 +63,14 @@ public class Pet extends BaseEntity {
 
 	public void setBirthDate(LocalDate birthDate) {
 		this.birthDate = birthDate;
+	}
+
+	public Set<Visit> getVisits() {
+		return visits;
+	}
+
+	public void setVisits(Set<Visit> visits) {
+		this.visits = visits;
 	}
 
 }
--- pet-clinic-data/src/main/java/com/zessin/springpetclinic/model/Visit.java
+++ pet-clinic-data/src/main/java/com/zessin/springpetclinic/model/Visit.java
@@ -2,10 +2,24 @@ package com.zessin.springpetclinic.model;
 
 import java.time.LocalDate;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "visit")
 public class Visit extends BaseEntity {
 
+	@Column(name = "date")
 	private LocalDate date;
+
+	@Column(name = "description")
 	private String description;
+
+	@ManyToOne
+	@JoinColumn(name = "pet_id")
 	private Pet pet;
 
 	public LocalDate getDate() {
